e456db2ebf13db499ef2977c0ff4b5ee57fca409
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-01-23 13:46:34 +0100

    Offert component added

---
+++ src/Components/Offert/Offert.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Offert.css';
+import search_icon from '../Assets/search.svg';
+import pig_icon from '../Assets/pig.svg';
+import safe_icon from '../Assets/safe.svg';
+
+
+const Offert = () => {
+  return (
+  <section class="offert">
+    <h2>Real estate agency you can trust</h2>
+    <div class="offert-container">
+      <div class="offert-box">
+        <div class="offert-box-image-container">
+          <img src={search_icon} alt="search" />
+        </div>
+        <h4>Personalized Search</h4>
+        <p>Find your dream apartment or home with us. We create a space where dreams of the perfect place become reality</p>
+      </div>
+      <div class="offert-box">
+        <div class="offert-box-image-container">
+          <img src={pig_icon} alt="search" />
+        </div>
+        <h4>Lowest Commissions</h4>
+        <p>Uncover your perfect apartment or home with us. We transform dreams into reality, with the lowest commissions.</p>
+      </div>
+      <div class="offert-box">
+        <div class="offert-box-image-container">
+          <img src={safe_icon} alt="search" />
+        </div>
+        <h4>Transaction Security</h4>
+        <p>Join us in realizing your dream home. With transaction security as a priority, we make dreams a reality.</p>
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Offert
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
+import Offert from '../Components/Offert/Offert';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
       <Hero />
+      <Offert />
     </div>
   )
 }
