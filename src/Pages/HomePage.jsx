d4cee3e6103e47fce4fc4ddab446eb296a25423e
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-01-23 13:51:52 +0100

    Steps component added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
 import Offert from '../Components/Offert/Offert';
+import Steps from '../Components/Steps/Steps';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
       <Hero />
       <Offert />
+      <Steps />
     </div>
   )
 }
