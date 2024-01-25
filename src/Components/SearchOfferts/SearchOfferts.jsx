78e6b5ef08bbe3ccd3b5c30939829861b2b63711
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-01-25 22:09:13 +0100

    sort element added in SearchOfferts component

--- src/Components/SearchOfferts/SearchOfferts.jsx
+++ src/Components/SearchOfferts/SearchOfferts.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import './SearchOfferts.css';
 import OffertItem from '../OffertItem/OffertItem';
+import arrow from '../Assets/arrow-searchbar.svg';
 
 const SearchOfferts = ({ filteredOffers }) => {
   return (
     <div className='searchofferts'>
-      <h2>Search property</h2>
-      <p className='searchofferts-resultcount'>{filteredOffers.length} results</p>
+      <div className="searchofferts-firstline">
+        <div className="searchofferts-title-count">
+          <h2>Search property</h2>
+          <p className='searchofferts-resultcount'>{filteredOffers.length} results</p>
+        </div>
+        <div className="searchofferts-sort-container">
+          <div>
+            <p>Sort</p>
+            <img src={arrow} alt="arrow" />
+          </div>
+          <ul>
+            <li>
+              <label>
+                <input type="radio" />
+                price descending
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type="radio" />
+                price ascending
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type="radio" />
+                surface descending
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type="radio" />
+                surface ascending
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className="searchpage-offerts">
         {filteredOffers.map((property, index) => (
           <OffertItem
