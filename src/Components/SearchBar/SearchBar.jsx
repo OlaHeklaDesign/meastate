65ab8b7449feeae1eaf398720bab2671afb9db65
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-01-25 21:51:07 +0100

    HTML structure of SearchBar change

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -9,134 +9,132 @@ const SearchBar = ({ filters, onFilterChange, onSearch }) => {
 
   return (
     <div className='searchbar'>
-      <div className="searchbar-inputs">
-        <div className="searchbar-offer-box">
+      <div className="searchbar-offer-container">
+        <div>
+          <p>buy</p>
+          <img src={arrow} alt="arrow" />
+        </div>
+        <ul>
+          <li>
+            <label>
+              <input  type="radio"
+                      name="service"
+                      value="rent"
+                      checked={selectedService === 'rent'}
+                      onChange={() => {
+                        setSelectedService('rent');
+                        onFilterChange({ target: { name: 'service', value: 'rent' } });
+                      }} />
+              Rent
+            </label>
+          </li>
+          <li>
+            <label>
+              <input  type="radio"
+                      name="service"
+                      value="sell"
+                      checked={selectedService === 'sell'}
+                      onChange={() => {
+                        setSelectedService('sell');
+                        onFilterChange({ target: { name: 'service', value: 'sell' } });
+                      }} />
+              Buy
+            </label>
+          </li>
+        </ul>
+      </div>
+      <div className="searchbar-location-container">
+        <p className='searchbar-title'>Location</p>
+        <input type="text" name="location" placeholder='Warsaw' value={filters.location} onChange={onFilterChange} />
+      </div>
+      <div className="searchbar-surface-container">
+        <div>
+          <p className='searchbar-title'>Surface Footage</p>
           <div>
-            <p>BUY</p>
+            <p className='searchbar-seletor-title'>40SQFT-70SQFT</p>
+            <img src={arrow} alt="" />
+          </div>
+        </div>
+        <ul>
+          <li>
+            <label>
+              <input  type="text"
+                      name="minSurface"
+                      placeholder='from'
+                      value={filters.minSurface}
+                      onChange={onFilterChange} />
+            </label>
+          </li>
+          <li>
+            <label>
+              <input  type="text"
+                      name="maxSurface"
+                      placeholder='to'
+                      value={filters.maxSurface}
+                      onChange={onFilterChange} />
+            </label>
+          </li>
+        </ul>
+      </div>
+      <div className="searchbar-market-container">
+        <div>
+          <p className='searchbar-title'>Market</p>
+          <div>
+            <p className='searchbar-seletor-title'>Primary</p>
             <img src={arrow} alt="arrow" />
           </div>
-          <ul className="searchbar-offer-select">
-            <li>
-              <label>
-                <input
-                  type="radio"
-                  name="service"
-                  value="rent"
-                  checked={selectedService === 'rent'}
-                  onChange={() => {
-                    setSelectedService('rent');
-                    onFilterChange({ target: { name: 'service', value: 'rent' } });
-                  }}
-                />
-                Rent
-              </label>
-            </li>
-            <li>
-              <label>
-                <input
-                  type="radio"
-                  name="service"
-                  value="sell"
-                  checked={selectedService === 'sell'}
-                  onChange={() => {
-                    setSelectedService('sell');
-                    onFilterChange({ target: { name: 'service', value: 'sell' } });
-                  }}
-                />
-                Sell
-              </label>
-            </li>
-          </ul>
         </div>
-        <div className="searchbar-location-box">
-          <p>Location</p>
-          <input type="text" name="location" placeholder='WARSAW' value={filters.location} onChange={onFilterChange} />
-        </div>
-        <div className="searchbar-surface-box">
+        <ul>
+          <li>
+            <label>
+              <input  type="radio"
+                      name="market"
+                      value="primary"
+                      checked={filters.market === 'primary'}
+                      onChange={onFilterChange} />
+              Primary
+            </label>
+          </li>
+          <li>
+            <label>
+              <input  type="radio"
+                      name="market"
+                      value="secondary"
+                      checked={filters.market === 'secondary'}
+                      onChange={onFilterChange} />
+              Secondary
+            </label>
+          </li>
+        </ul>
+      </div>
+      <div className="searchbar-price-container">
+        <div>
+          <p className='searchbar-title'>Price</p>
           <div>
-            <p>square footage</p>
-            <div>
-              <p>50sqft-70sqft</p>
-              <img src={arrow} alt="arrow" />
-            </div>
-          </div>
-          <div className="searchbar-select-surface">
-            <input
-              type="text"
-              name="minSurface"
-              placeholder='from'
-              value={filters.minSurface}
-              onChange={onFilterChange}
-            />
-            <input
-              type="text"
-              name="maxSurface"
-              placeholder='to'
-              value={filters.maxSurface}
-              onChange={onFilterChange}
-            />
+            <p className='searchbar-seletor-title'>400K - 700K</p>
+            <img src={arrow} alt="" />
           </div>
         </div>
-        <div className="searchbar-market-box">
-          <div>
-            <p>market</p>
-            <div>
-              <p>primary</p>
-              <img src={arrow} alt="arrow" />
-            </div>
-          </div>
-          <div className="searchbar-offer-select">
-            <li>
-              <label>
-                <input
-                  type="radio"
-                  name="market"
-                  value="primary"
-                  checked={filters.market === 'primary'}
-                  onChange={onFilterChange}
-                />
-                Primary
-              </label>
-            </li>
-            <li>
-              <label>
-                <input
-                  type="radio"
-                  name="market"
-                  value="secondary"
-                  checked={filters.market === 'secondary'}
-                  onChange={onFilterChange}
-                />
-                Secondary
-              </label>
-            </li>
-          </div>
-        </div>
-        <div className="searchbar-price-box">
-          <div>
-            <p>price</p>
-            <div>
-              <p>500K - 700K</p>
-              <img src={arrow} alt="arrow" />
-            </div>
-          </div>
-          <div className='search-price-inputs'>
-            <input
-              type="text"
-              name="minPrice"
-              placeholder='from'
-              value={filters.minPrice}
-              onChange={onFilterChange}
-            />
-            <input
-              type="text"
-              name="maxPrice"
-              placeholder='to'
-              value={filters.maxPrice}
-              onChange={onFilterChange}
-            />
-          </div>
-        </div>
+        <ul>
+          <li>
+            <label>
+              <input  type="text"
+                      name="minPrice"
+                      placeholder='from'
+                      value={filters.minPrice}
+                      onChange={onFilterChange} />
+            </label>
+          </li>
+          <li>
+            <label>
+              <input  type="text"
+                      name="maxPrice"
+                      placeholder='to'
+                      value={filters.maxPrice}
+                      onChange={onFilterChange} />
+            </label>
+          </li>
+        </ul>
       </div>
       <button onClick={onSearch}>search</button>
     </div>
